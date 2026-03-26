9e42314a0563652c23b5715981c85871bd2f8705
yld0224 <yld0224@sjtu.edu.cn>
2026-03-26 22:08:22 +0800

    Changed ptr design & Finished rotation functions

--- RBTree.hpp
+++ RBTree.hpp
@@ -11,26 +11,48 @@ class RBTree{
 public:
     struct Node{
         T value;
-        std::shared_ptr<Node> parent;
-        std::shared_ptr<Node> leftChild;
-        std::shared_ptr<Node> rightChild;
+        Node* parent = nullptr;
+        std::unique_ptr<Node> leftChild;
+        std::unique_ptr<Node> rightChild;
         bool isRed = false;
         size_t subTreeSize = 0;
 
-        const bool isLeftChild() const {return this == parent -> leftChild;}
-        const bool isRightChild() const {return this == parent -> rightChild;}
-        explicit Node(std::shared_ptr<Node> pa) : parent(std::move(pa)) {}
+        const bool isLeftChild() const {return parent && this == parent -> leftChild.get();}
+        const bool isRightChild() const {return parent && this == parent -> rightChild.get();}
+        explicit Node(Node* pa) : parent(pa) {}
         Node() = default;
     };
 private:
-    std::shared_ptr<Node> root;
+    std::unique_ptr<Node> root;
     size_t size = 0;
     Compare cmp = Compare();
 public:
     RBTree() = default;
-    void rotateRight(std::shared_ptr<Node> nd){}
-    void rotateLeft(std::shared_ptr<Node> nd){}
-    ~RBTree(){}
+    ~RBTree() = default;//由于使用了unique_ptr,在析构的时候删除root会递归删除树的其他部分
+    void rotateRight(std::unique_ptr<Node>& nd){//引用unique_ptr,防止转移所有权到局部
+        assert(nd -> leftChild);
+        std::unique_ptr<Node> l = std::move(nd -> leftChild);
+        l -> parent = nd.parent;
+        nd -> parent = l.get();
+        if (l -> rightChild) {
+            l -> rightChild -> parent = nd.get();
+        }
+        nd -> leftChild = std::move(l -> rightChild);
+        l -> rightChild = std::move(nd);
+        nd = std::move(l);//把node重新连接回树,否则node的parent直接丢失了node
+    }
+    void rotateLeft(std::unique_ptr<Node>& nd){
+        assert(nd -> rightChild);
+        std::unique_ptr<Node> r = std::move(nd -> rightChild);
+        r -> parent = nd.parent;
+        nd -> parent = r.get();
+        if (r -> leftChild) {
+            r -> leftChild -> parent = nd.get();
+        }
+        nd -> rightChild = std::move(r -> leftChild);
+        r -> leftChild = std::move(nd);
+        nd = std::move(r);//把node重新连接回树,否则node的parent直接丢失了node
+    }
 };
 
 }
